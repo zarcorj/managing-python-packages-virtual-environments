3b3f9d2d146b015c7f162028e3b691fcb522c561
Javier Zarco Rebollar <zarcorx@gmail.com>
2024-10-20 22:21:45 -0600

    src: add Google format docstrings

--- 03/demos/m3_venv/get_rates.py
+++ 03/demos/m3_venv/get_rates.py
@@ -1,7 +1,31 @@
+"""
+Fetches the latest exchange rate from Euro to USD and prints it.
+
+This script sends a GET request to the Exchange Rates API to retrieve the 
+latest exchange rate from Euro to USD. The response is parsed and the 
+exchange rate is printed.
+
+Modules:
+    requests: To send HTTP requests.
+    box: To convert the JSON response to a Python object.
+
+Usage:
+    Run the script to fetch and print the latest exchange rate from Euro to USD.
+
+Example:
+    $ python get_rates.py
+    1 Euro is 1.12 dollars.
+
+Attributes:
+    response (requests.Response): The response object from the GET request.
+    b (Box): The parsed JSON response as a Box object.
+"""
+
 import requests
 from box import Box
 
 response = requests.get(
     "https://api.exchangeratesapi.io/latest?symbols=USD")
 b=Box(response.json())
-print("1 Euro is ", b.rates.USD, " dollars.")
+#print("1 Euro is ", b.rates.USD, " dollars.")
+print(b)
